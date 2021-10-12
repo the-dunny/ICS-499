9dfb2affc5738979fd0e63efa77c54a8c21adf49
Loreq <33507383+Loreq@users.noreply.github.com>
2021-10-12 11:34:51 -0500

    fixed merge

--- src/main/java/application/timer/Clock.java
+++ src/main/java/application/timer/Clock.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package application.timer;
 
 import java.beans.*;
@@ -34,40 +33,3 @@ public class Clock implements Runnable {
 	} catch (InterruptedException ie) {}
     }
 }
-=======
-package application.timer;
-
-import java.beans.*;
-
-public class Clock implements Runnable {
-    private Thread thread = new Thread(this);
-    private static Clock clock;
-    private final PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
-
-    public void addPropertyChangeListener(PropertyChangeListener listener) {
-	this.propertyChangeSupport.addPropertyChangeListener(listener);
-    }
-
-    public void removePropertyChangeListener(PropertyChangeListener listener) {
-	this.propertyChangeSupport.removePropertyChangeListener(listener);
-    }
-
-    private Clock() {
-	thread.start();
-    }
-
-    public static Clock instance() {
-	if (clock == null) clock = new Clock();
-	return clock;
-    }
-
-    public void run() {
-	try {
-	    while (true) {
-		Thread.sleep(1000);
-		this.propertyChangeSupport.firePropertyChange(null, null, null);
-	    }
-	} catch (InterruptedException ie) {}
-    }
-}
->>>>>>> 660f5ff24e83d3d3fabf28a71c42e1b862a7df40
