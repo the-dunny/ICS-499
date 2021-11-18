f06ba30c222eb3f2cf0ff8031f1463a899fbd84a
the-dunny <jordandodd8184@gmail.com>
2021-11-18 12:13:07 -0600

    starting role based security for -admin- page

--- src/main/resources/static/src/app/app.component.ts
+++ src/main/resources/static/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import { AuthServiceService } from './services/auth-service/auth-service.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  isLoggedIn(): boolean{
+    let x = sessionStorage.getItem('authenticatedUser');
+    if(x === null) return true
+    return false
+  }
+
   title = 'LineGame';
 }
